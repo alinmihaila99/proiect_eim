7d2cffb3f4a6c6f3f5b0c1530da7d524371424a5
alinmihaila99 <mihaila.alin99@yahoo.com.com>
2022-05-16 15:29:50 +0300

    Added repo

--- app/src/main/java/com/example/myquizzkotlin/repository/Repository.kt
+++ app/src/main/java/com/example/myquizzkotlin/repository/Repository.kt
@@ -2,13 +2,19 @@ package com.example.myquizzkotlin.repository
 
 import com.example.myquizzkotlin.api.RetrofitInstance
 import com.example.myquizzkotlin.model.Profile
+import com.example.myquizzkotlin.model.Question
 import com.example.myquizzkotlin.model.UserCredentials
 
 class Repository {
     suspend fun login(userCredentials: UserCredentials): Profile{
+        if(userCredentials.email == "" || userCredentials.password == "")
+            return Profile(-1, -1, -1);
         return RetrofitInstance.api.login(userCredentials)
     }
     suspend fun register(userCredentials: UserCredentials): Profile{
+        if(userCredentials.email == "" || userCredentials.password == "")
+            return Profile(-1, -1, -1);
         return RetrofitInstance.api.register(userCredentials)
     }
+
 }
