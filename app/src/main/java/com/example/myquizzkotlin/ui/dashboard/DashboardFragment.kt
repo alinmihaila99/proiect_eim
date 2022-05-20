2285991cbca67136b1cbda2f4c360102df4cb8b8
alinmihaila99 <mihaila.alin99@yahoo.com.com>
2022-05-20 12:11:18 +0300

    Updated the fragments

--- app/src/main/java/com/example/myquizzkotlin/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myquizzkotlin/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,8 @@
 package com.example.myquizzkotlin.ui.dashboard
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myquizzkotlin.databinding.FragmentDashboardBinding
+import android.widget.*
+import android.widget.RadioGroup
+import com.example.myquizzkotlin.api.RetrofitInstance
+import com.example.myquizzkotlin.model.Question
+import com.example.myquizzkotlin.model.UpdateScore
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DashboardFragment : Fragment() {
 
@@ -32,6 +42,144 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val call = RetrofitInstance.api.getQuestions();
+        val settings = requireContext().getSharedPreferences("dailyGoal", 0)
+        val userId = settings.getInt("logedinUserId", 2)
+
+        var count = 0;
+        var score = 0;
+        var questionCount = 1;
+        val radio_group = binding.radioGroup;
+
+        val radioButton1 = binding.red;
+        val radioButton2 = binding.green;
+        val radioButton3 = binding.yellow;
+        val radioButton4 = binding.pink;
+        val nextButton = binding.button5;
+        val question = binding.title;
+        val scoreView = binding.textView2;
+        val mTextField = binding.textView3;
+        var flag = false;
+
+        object : CountDownTimer(30000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                questionCount = 5;
+                mTextField.setText("done!")
+            }
+        }.start()
+
+        var questionsList:MutableList<Question> = mutableListOf();
+        call.enqueue(object: Callback<MutableList<Question>>{
+            override fun onFailure(call: Call<MutableList<Question>>, t: Throwable) {
+                Log.e("Fail!!", "Fail####" + t.message)
+            }
+
+            override fun onResponse(
+                call: Call<MutableList<Question>>,
+                response: Response<MutableList<Question>>
+            ) {
+                if(response.isSuccessful()){
+                    val questionsFromDatabase = response.body();
+                    questionsList = questionsFromDatabase!!;
+                    println(questionsFromDatabase?.get(0));
+                    println(questionsList);
+                    question.text = questionsList[0].question;
+                    radioButton1.text = questionsList[0].response1;
+                    radioButton2.text = questionsList[0].response2;
+                    radioButton3.text = questionsList[0].response3;
+                    radioButton4.text = questionsList[0].response4;
+                }
+            }
+        })
+
+        fun getCorrectAnswerForQuestion(question: Question): String {
+            return question.correctResponse
+        }
+
+        nextButton.setOnClickListener {
+            Log.d("questionCount", questionCount.toString())
+            if (questionCount < 5) {
+                radioButton1.text = questionsList[questionCount].response1
+                radioButton2.text = questionsList[questionCount].response2
+                radioButton3.text = questionsList[questionCount].response3
+                radioButton4.text = questionsList[questionCount].response4
+                question.text = questionsList[questionCount].question
+                questionCount++;
+                count++
+                radio_group.clearCheck()
+            }
+
+            if (questionCount == 5) {
+                nextButton.text = "FINISH QUIZ";
+                questionCount++;
+            } else if (questionCount == 6) {
+                println(score);
+                scoreView.text = "Scorul tau este:  " + score;
+                if(!flag) {
+                    val callUpdateScore =
+                        RetrofitInstance.api.updateScore(UpdateScore(score, userId))
+                    callUpdateScore.enqueue(object : Callback<String> {
+                        override fun onFailure(call: Call<String>, t: Throwable) {
+                            Log.e("Fail!!", "Fail####" + t.message)
+                        }
+
+                        override fun onResponse(call: Call<String>, response: Response<String>) {
+                            TODO("Not yet implemented")
+                        }
+                    })
+                }
+                flag = true;
+            }
+        }
+
+        radio_group.setOnCheckedChangeListener { radioGroup, i ->
+            run {
+                val radio: RadioButton = root.findViewById(binding.radioGroup.checkedRadioButtonId)
+                Toast.makeText(
+                    this.context, "On click : ${radio.text}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        // Get radio group selected item using on checked change listener
+        radio_group.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener { group, checkedId ->
+                if (checkedId != -1 && questionsList.size > 0)
+                {
+                    val radio: RadioButton = root.findViewById(checkedId)
+//                    Toast.makeText(this.context,"Raspuns selectat: "+
+//                            " ${radio.text}",
+//                        Toast.LENGTH_SHORT).show()
+                    if(radio.text == questionsList[count].correctResponse){
+                        println("Raspuns corect:" + radio.text);
+                        score ++;
+                    }
+                }
+            })
+        // Get radio group selected status and text using button click event
+
+//        button.setOnClickListener{
+//            // Get the checked radio button id from radio group
+//            var id: Int = radio_group.checkedRadioButtonId
+//            if (id!=-1){ // If any radio button checked from radio group
+//                // Get the instance of radio button using id
+//                val radio:RadioButton = root.findViewById(id)
+//                Toast.makeText(this.context,"On button click :" +
+//                        " ${radio.text}",
+//                    Toast.LENGTH_SHORT).show()
+//            }else{
+//                // If no radio button checked in this radio group
+//                Toast.makeText(this.context,"On button click :" +
+//                        " nothing selected",
+//                    Toast.LENGTH_SHORT).show()
+//            }
+//        }
         return root
     }
 
--- app/src/main/java/com/example/myquizzkotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myquizzkotlin/ui/home/HomeFragment.kt
@@ -1,12 +1,18 @@
 package com.example.myquizzkotlin.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.myquizzkotlin.AlreadyCompletedDailyGoal
+import com.example.myquizzkotlin.DailyGoal
+import com.example.myquizzkotlin.LeaderBoard
+import com.example.myquizzkotlin.R
 import com.example.myquizzkotlin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -31,6 +37,30 @@ class HomeFragment : Fragment() {
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        val startQuizzButton: Button = root.findViewById(R.id.button2);
+        val seeDailyGoalButton: Button = root.findViewById(R.id.button3);
+        val seeLeaderbordButton: Button = root.findViewById(R.id.button4);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+//        startQuizzButton.setOnClickListener { println("Start QUIZ!!!!!!!!") }
+//        seeDailyGoalButton.setOnClickListener { println("Am apasat Daily Goal") }
+        seeLeaderbordButton.setOnClickListener { val intent = Intent(context, LeaderBoard::class.java)
+            startActivity(intent) }
+        val settings = context?.getSharedPreferences("dailyGoal", 0)
+        val dailyGoal = settings?.getInt("dailyGoal", 0)
+        println("Daily goal is: ")
+        println(dailyGoal);
+        if(dailyGoal == 1){
+            seeDailyGoalButton.setOnClickListener {  val intent = Intent(context, AlreadyCompletedDailyGoal::class.java)
+                startActivity(intent) }
+        }
+        else{
+            seeDailyGoalButton.setOnClickListener {  val intent = Intent(context, DailyGoal::class.java)
+                startActivity(intent) }
         }
         return root
     }
