d9406d19b8a8778e433193c0270d9eb740725449
alinmihaila99 <mihaila.alin99@yahoo.com.com>
2022-05-29 21:06:33 +0300

    Final commit

--- app/src/test/java/com/example/myquizzkotlin/RequestTest.kt
+++ app/src/test/java/com/example/myquizzkotlin/RequestTest.kt
@@ -1,17 +1,157 @@
 package com.example.myquizzkotlin
 
+import android.util.Log
+import com.example.myquizzkotlin.api.RetrofitInstance
+import com.example.myquizzkotlin.model.Profile
+import com.example.myquizzkotlin.model.Question
+import com.example.myquizzkotlin.model.UserCredentials
+import com.example.myquizzkotlin.repository.Repository
+import io.mockk.every
+import io.mockk.mockkObject
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
+import retrofit2.Call
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class RequestTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun loginTest() {
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val user: Profile = runBlocking { RetrofitInstance.api.login(UserCredentials
+            ("alinmihaila", "parola")) }
+        assertEquals(user.id, 1);
+        assertEquals(user.quizesSolved, 30);
+        assertEquals(user.dailyGoal, 1);
+    }
+
+    @Test
+    fun registerTest(){
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val user: Profile = runBlocking { RetrofitInstance.api.register(UserCredentials
+            ("alinmihaila", "parola")) }
+        assertEquals(user.id, 1);
+        assertEquals(user.quizesSolved, 30);
+        assertEquals(user.dailyGoal, 1);
+    }
+
+    @Test
+    fun testGetQuestions(){
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val questions: Call<MutableList<Question>> = RetrofitInstance.api.getQuestions();
+        val questionsList = questions.execute();
+
+        assertEquals(questionsList.body()?.size ?: 0, 3)
+    }
+
+    //TEST REPOSITORY CLASS
+
+    @Test
+    fun loginTestFromRepository() {
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val repostitory: Repository = Repository();
+        val user: Profile = runBlocking { repostitory.login(UserCredentials
+            ("alinmihaila", "parola")) }
+        assertEquals(user.id, 1);
+        assertEquals(user.quizesSolved, 30);
+        assertEquals(user.dailyGoal, 1);
+
+    }
+
+    @Test
+    fun loginTestFromRepositoryWithEmptyPasswordOrUsername() {
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val repostitory: Repository = Repository();
+        val user: Profile = runBlocking { repostitory.login(UserCredentials
+            ("", "")) }
+        assertEquals(user.id, -1);
+        assertEquals(user.quizesSolved, -1);
+        assertEquals(user.dailyGoal, -1);
+
+    }
+
+    @Test
+    fun registerTestFromRepository() {
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val repostitory: Repository = Repository();
+        val user: Profile = runBlocking {
+            repostitory.register(
+                UserCredentials
+                    ("alinmihaila", "parola")
+            )
+        }
+        assertEquals(user.id, 1);
+        assertEquals(user.quizesSolved, 30);
+        assertEquals(user.dailyGoal, 1);
+    }
+
+    @Test
+    fun registerTestFromRepositoryWithEmptyPasswordOrUsername() {
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val repostitory: Repository = Repository();
+        val user: Profile = runBlocking {
+            repostitory.register(
+                UserCredentials
+                    ("", "")
+            )
+        }
+        assertEquals(user.id, -1);
+        assertEquals(user.quizesSolved, -1);
+        assertEquals(user.dailyGoal, -1);
+    }
+
+    @Test
+    fun testGetCorrectAnswer(){
+
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val questions: Call<MutableList<Question>> = RetrofitInstance.api.getQuestions();
+        val questionsList = questions.execute();
+        val q:Question? = questionsList.body()?.get(0);
+        if (q != null) {
+            assertEquals(q.correctResponse, "raspuns2")
+        }
+    }
+
+    @Test
+    fun testGetLeaderboard(){
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val leaderBoard: Call<MutableList<String>> = RetrofitInstance.api.getLeaderboard();
+        val questionsList = leaderBoard.execute();
+
+        assertEquals(questionsList.body()?.size ?: 0, 3)
+    }
+
+    @Test
+    fun testGetLeaderboardOrder(){
+        mockkObject(RetrofitInstance);
+        every { RetrofitInstance getProperty "api" } returns TestClass.createMockAPI();
+        val leaderBoard: Call<MutableList<String>> = RetrofitInstance.api.getLeaderboard();
+        val leaderboardList = leaderBoard.execute();
+
+        assertEquals(leaderboardList.body()?.get(0) ?: 0, "1, mihai, 30")
+    }
+
+    @Test
+    fun testGetUserPosition(){
+        mockkObject(RetrofitInstance);
+        every {RetrofitInstance getProperty "api"} returns TestClass.createMockAPI();
+        val position: Call<String> = RetrofitInstance.api.getPosition("9");
+        val userPosition = position.execute();
+
+        assertEquals(userPosition.body(), "2")
     }
 }
--- app/src/test/java/com/example/myquizzkotlin/TestClass.kt
+++ app/src/test/java/com/example/myquizzkotlin/TestClass.kt
@@ -1,4 +1,170 @@
 package com.example.myquizzkotlin
 
+import com.example.myquizzkotlin.api.ApiRequest
+import com.example.myquizzkotlin.model.*
+import okhttp3.Request
+import okio.Timeout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class TestClass {
+    companion object{
+
+        fun createMockQuestions():MutableList<Question>{
+            return mutableListOf(
+                Question("Intrebare2", "raspuns1", "raspuns2",
+            "raspuns3", "raspuns4", "raspuns2"),
+                (Question("Intrebare2", "raspuns1", "raspuns2",
+                    "raspuns3", "raspuns4", "raspuns4")),
+                (Question("Intrebare3", "raspuns1", "raspuns2",
+                    "raspuns3", "raspuns4", "raspuns1"))
+                )
+        }
+
+        fun createMockLeaderboard():MutableList<String>{
+            return mutableListOf(
+                "1, mihai, 30",
+                "2, iulia, 20",
+                "3, cristian, 14"
+            )
+        }
+
+        fun mockUserPosition(): String{
+            return "2";
+        }
+
+        fun createMockAPI(): ApiRequest{
+            return object :ApiRequest{
+                override suspend fun login(userCredentials: UserCredentials): Profile {
+                    return Profile(1, 30, 1);
+                }
+
+                override suspend fun register(userCredentials: UserCredentials): Profile {
+                    return Profile(1, 30, 1);
+                }
+
+                override fun getQuestions(): Call<MutableList<Question>> {
+                    return object:Call<MutableList<Question>>{
+                        override fun clone(): Call<MutableList<Question>> {
+                            return this;
+                        }
+
+                        override fun execute(): Response<MutableList<Question>> {
+                            return Response.success(createMockQuestions());
+                        }
+
+                        override fun enqueue(callback: Callback<MutableList<Question>>) {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun isExecuted(): Boolean {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun cancel() {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun isCanceled(): Boolean {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun request(): Request {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun timeout(): Timeout {
+                            TODO("Not yet implemented")
+                        }
+
+                    }
+                }
+
+                override fun getLeaderboard(): Call<MutableList<String>> {
+                    return object:Call<MutableList<String>>{
+                        override fun clone(): Call<MutableList<String>> {
+                            return this;
+                        }
+
+                        override fun execute(): Response<MutableList<String>> {
+                            return Response.success(createMockLeaderboard())
+                        }
+
+                        override fun enqueue(callback: Callback<MutableList<String>>) {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun isExecuted(): Boolean {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun cancel() {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun isCanceled(): Boolean {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun request(): Request {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun timeout(): Timeout {
+                            TODO("Not yet implemented")
+                        }
+
+                    }
+                }
+
+                override fun getPosition(userId: String): Call<String> {
+                    return object: Call<String>{
+                        override fun clone(): Call<String> {
+                            return this;
+                        }
+
+                        override fun execute(): Response<String> {
+                            return Response.success(mockUserPosition());
+                        }
+
+                        override fun enqueue(callback: Callback<String>) {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun isExecuted(): Boolean {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun cancel() {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun isCanceled(): Boolean {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun request(): Request {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun timeout(): Timeout {
+                            TODO("Not yet implemented")
+                        }
+
+                    }
+                }
+
+                override fun updateScore(updateUserScore: UpdateScore): Call<String> {
+                    TODO("Not yet implemented")
+                }
+
+                override fun updateDaily(userId: UserId): Call<UserId> {
+                    TODO("Not yet implemented")
+                }
+
+            }
+
+        }
+    }
 }
